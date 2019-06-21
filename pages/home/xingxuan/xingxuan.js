28acda145d510dcd7fbba863b6357ef76375fd51
chenfeixin <18825481575@163.com>
2019-06-21 11:50:28 +0800

    1.01

---
+++ pages/home/xingxuan/xingxuan.js
@@ -0,0 +1,90 @@
+// pages/home/xingxuan/xingxuan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+      name: '祖马龙礼盒香水香薰',
+      price: '￥1,700'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }, {
+      name: 'Dyson戴森 吹风机',
+      price: '￥2,950'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
