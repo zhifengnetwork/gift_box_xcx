fa12823a65ebed9f921b7acad1f490d6a50ff231
FZJ <yudwcom@163.com>
2019-07-16 12:27:01 +0800

    小程序登录

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ App({
   onLaunch: function () {
 
 
-
     // 判断设备是否为 iPhone X
     this.checkIsIPhoneX();
 
@@ -16,33 +15,41 @@ App({
       }
     })
 
-
   },
 
   // 获取用户信息
   getUserInfo: function (cb) {
 
-    if (this.globalData.userInfo) {
+    //如果数组是空的
+    if (this.globalData.userInfo.length == 0) {
+
+      var that = this
+      // 登录
+      wx.login({
+        success: res => {
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          api.getJSON('api/login/index?code=' + res.code, function (res) {
+            if (res.data.status == 1) {
+
+              that.globalData.userInfo = res.data.data
+              that.globalData.token = res.data.data.token
+
+              //回调
+              cb && cb(that.globalData.userInfo)
+              console.log('登录成功')
+              console.log(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+
+    } else {
+
+      console.log('已登录')
       cb && cb(this.globalData.userInfo)
+      return true;
+
     }
-
-    var that = this
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        api.getJSON('api/login/index?code=' + res.code, function (res) {
-          if (res.data.status == 1) {
-            that.globalData.userInfo = res.data.data
-            that.globalData.token = res.data.data.token
-            //回调
-            cb && cb(that.globalData.userInfo)
-
-          }
-        })
-      }
-    })
-
   },
 
   //判断设备是否为 iPhone X
@@ -53,7 +60,7 @@ App({
         // 根据 model 进行判断
         if (res.model.search('iPhone X') != -1) {
           self.globalData.isIPX = true;
-          console.log("5555555555555")
+
         }
         // 或者根据 screenHeight 进行判断
         // if (res.screenHeight == 812) {
@@ -67,8 +74,7 @@ App({
 
   globalData: {
     userInfo: [],
-    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEQyIsImlhdCI6MTU2MzE1NTUyMSwiZXhwIjoxNTYzMTkxNTIxLCJ1c2VyX2lkIjo4OX0.9bCLCxQnn_YPy58A2YVHSwOZ8sKq1f6w5jLEuez69Rs',
-
+    token: '',
     height: 0
   }
 
