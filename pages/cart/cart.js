407e8b6b6e65603e111b5fedd270d83f7d7cf593
ZZZhangYunfei <86975943@qq.com>
2019-07-18 15:42:27 +0800

    购物车

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,36 +1,93 @@
-var api = require('../../utils/api.js');
-var app = getApp();
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    carts: [],
-    goodsList: [],
-    empty: true,
-    minusStatuses: ['disabled', 'disabled', 'normal', 'normal', 'disabled'],
-    selectedAllStatus: true,
-    total: '',
-    
-    items: [
-      { name: 'USA', value: '美国' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
-      { name: 'JPN', value: '日本' },
-      { name: 'ENG', value: '英国' },
-      { name: 'TUR', value: '法国' },
-    ]
+    carts: [
+      { id: 1, title: '新鲜芹菜 半斤', image: '../public/images/cart/yifu.jpg', num: 4, price: 0.01, selected: true },
+      { id: 2, title: '素米 500g', image: '../public/images/cart/yifu.jpg', num: 1, price: 0.03, selected: true }
+      ],               // 购物车列表
+    hasList: false,          // 列表是否有数据
+    totalPrice: 0,           // 总价，初始为0
+    selectAllStatus: true,    // 全选状态，默认全选
+    items: [],
+    startX: 0, //开始坐标
+    startY: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-
+    console.log(this.data.carts)
+      this.data.items.push({
+        content:this.data.carts,
+        isTouchMove: false //默认全隐藏删除
+      })
+    console.log(this.data.items[0].content)
+    this.setData({
+      items: this.data.carts
+    })
   },
-
+  //手指触摸动作开始 记录起点X坐标
+  touchstart: function (e) {
+    //开始触摸时 重置所有删除
+    this.data.items.forEach(function (v, i) {
+      if (v.isTouchMove)//只操作为true的
+        v.isTouchMove = false;
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      items: this.data.items
+    })
+  }, 
+  //滑动事件处理
+  touchmove: function (e) {
+      var that = this,
+      index = e.currentTarget.dataset.index,//当前索引
+      startX = that.data.startX,//开始X坐标
+      startY = that.data.startY,//开始Y坐标
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+      //获取滑动角度
+    angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+    that.data.items.forEach(function (v, i) {
+      v.isTouchMove = false
+      //滑动超过30度角 return
+      if (Math.abs(angle) > 30) return;
+      if (i == index) {
+        if (touchMoveX > startX) //右滑
+          v.isTouchMove = false
+        else //左滑
+          v.isTouchMove = true
+      }
+    })
+    //更新数据
+    that.setData({
+      items: that.data.items
+    })
+  },
+  /**
+  * 计算滑动角度
+  * @param {Object} start 起点坐标
+  * @param {Object} end 终点坐标
+  */
+  angle: function (start, end) {
+    var _X = end.X - start.X,
+      _Y = end.Y - start.Y
+    //返回角度 /Math.atan()返回数字的反正切值
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+  },
+  //删除事件
+  del: function (e) {
+    this.data.items.splice(e.currentTarget.dataset.index, 1)
+    this.setData({
+      items: this.data.items
+    })
+  } , 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -42,44 +99,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var minusStatuses = [];
-    var that = this
-    
-    api.getJSON('api/cart/cartlist?token='+app.globalData.token,function(res){
-      
-      console.log(res.data.data)
-
-      var carts = res.data.data
-      
-      if (carts.length != 0)
-        that.setData({ empty: false });
-      else {
-        that.setData({ empty: true });
-      }
-
-      var selectedAllStatus = true;
-      for (var i = 0; i < carts.length; i++) {
-        //var goods = carts[i].get('goods');
-        //goodsList[i] = goods;
-        //carts[i].selected = true;
-        if (carts[i].selected == 1)
-          carts[i].selected = true;
-        else {
-          carts[i].selected = false;
-          selectedAllStatus = false;
-        }
-        minusStatuses[i] = 1;//carts[i].get('quantity') <= 1 ? 'disabled' : 'normal';
-      }
-      // console.log(carts);
-      that.setData({
-        carts: carts,
-        selectedAllStatus: selectedAllStatus,
-        //goodsList: goodsList,
-        minusStatuses: minusStatuses
-      });
-
-    })
-
+    this.setData({
+      hasList: true,        // 既然有数据了，那设为true吧
+    });
   },
 
   /**
@@ -109,21 +131,98 @@ Page({
   onReachBottom: function () {
     
   },
-
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
     
   },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  // 计算总价
+  getTotalPrice() {
+    let carts = this.data.carts;                  // 获取购物车列表
+    let total = 0;
+    for (let i = 0; i < carts.length; i++) {         // 循环列表得到每个数据
+      if (carts[i].selected) {                   // 判断选中才会计算价格
+        total += carts[i].num * carts[i].price;     // 所有价格加起来
+      }
+    }
+    this.setData({                                // 最后赋值到data中渲染到页面
+      carts: carts,
+      totalPrice: total.toFixed(2)
+    });
   },
-  
-  see:function(e){
-    wx.switchTab({
-      url: '../index/index',
-    })
+  // 选择事件
+  selectList(e) {
+    const index = e.currentTarget.dataset.index;    // 获取data- 传进来的index
+    let carts = this.data.carts;                    // 获取购物车列表
+    const selected = carts[index].selected;         // 获取当前商品的选中状态
+    carts[index].selected = !selected;              // 改变状态
+    this.setData({items: carts});
+    this.getTotalPrice();                           // 重新获取总价
+    
+
+  },
+  //全选事件
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus;    // 是否全选状态
+    selectAllStatus = !selectAllStatus;
+    let carts = this.data.carts;
+
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus;            // 改变所有商品状态
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    });
+    this.getTotalPrice();                               // 重新获取总价
+  },
+  // 增加数量
+  addCount(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+   
+    this.setData({
+      items: carts
+    });
+    this.getTotalPrice();
+  },
+  // 减少数量
+  minusCount(e) {
+    console.log("ssssssss")
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if (num <= 1) {
+      return false;
+    }
+    num = num - 1;
+    carts[index].num = num;
+    console.log(carts)
+    this.setData({
+      items: carts
+    });
+    console.log(this.data.carts)
+    this.getTotalPrice();
+  },
+  // 删除商品
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index;
+    let carts = this.data.carts;
+    carts.splice(index, 1);              // 删除购物车列表里这个商品
+    this.setData({
+      items: carts
+    });
+    if (!carts.length) {                  // 如果购物车为空
+      this.setData({
+        hasList: false              // 修改标识为false，显示购物车为空页面
+      });
+    } else {                              // 如果不为空
+      this.getTotalPrice();           // 重新计算总价格
+    }
   }
 
 })
