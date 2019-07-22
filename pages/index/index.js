6f77759d5368ddb60572f747b6de00a6eb54498d
ZZZhangYunfei <86975943@qq.com>
2019-07-22 14:22:59 +0800

    商品详情

--- pages/index/index.js
+++ pages/index/index.js
@@ -30,11 +30,26 @@ Page({
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
     indicatorDots: false,
-    autoplay: false,
+    // autoplay: false,
     interval: 5000,
     duration: 1000,
     imgUrlslength: '',
-    swipernow: 1
+    swipernow: 1,
+    imgs: [
+
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+
+    ],
+
+    // currentSwiper: 0,
+    autoplay: true
+
+
+
+
+
 
   },
   onLoad: function (options) {
