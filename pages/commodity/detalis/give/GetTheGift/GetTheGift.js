93313ae585119e057be70707b5e054f587687b24
ICEY-I-SEE <1141839113@qq.com>
2019-07-26 15:36:40 +0800

    ⑨

--- pages/commodity/detalis/give/GetTheGift/GetTheGift.js
+++ pages/commodity/detalis/give/GetTheGift/GetTheGift.js
@@ -7,16 +7,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    flag: true
+    flag: true,
+    goods:'',
+    address:'',
+    active:true
   },
   show: function () {
+    if (!this.data.active){
+      wx.showToast({
+        icon: 'none',
+        title: '该订单已经领取过啦!',
+        duration: 2500
+      })
+      return false;
+    }
     this.setData({ flag: false })
   },
   hide: function () {
-    console.log(666)
     this.setData({ flag: true })
   },
-
+  again:function(){
+    wx.navigateTo({
+      url: '../../../../site/site?again='+true,
+    })
+  },
   tiaozhuan: function () {
     wx.navigateTo({
       url: '../GetTheSuccess/success',
@@ -27,13 +41,42 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    api.postJSON('api/order/order_detail',{
+    let that = this;
+    let address_id = '';
+    console.log(options)
+    if (options.address_id){
+      address_id = options.address_id;
+      api.postJSON('api/gift/set_address',{
+        'token': app.globalData.token,
+        'addressid': address_id,
+        'joinid':'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJEQyIsImlhdCI6MTU2NDEyNTA4OCwiZXhwIjoxNTY0MTYxMDg4LCJ1c2VyX2lkIjoyODM1fQ.RPqQUSTJ1Ip_U9k2jB94oOHB-0AFU5ccT-DMXaEqRU8'
+      },
+      function(res){
+
+      })
+    }
+    api.postJSON('api/gift/share_callback',{
       'token': app.globalData.token,
       // 'order_id': options.order_id,
-      'order_id': options.order_id,
+      'order_id': 2835,
     },
     function(res){
-      console.log(res)
+      if(res.data.status==1){
+        that.setData({
+          goods: res.data.data.goods,
+          address: res.data.data.address
+        })
+      }else{
+        wx.showToast({
+          icon: 'none',
+          title: res.data.msg,
+          duration: 2500
+        })
+        that.setData({
+          active:false
+        })
+      }
+      console.log(res.data)
     })
   },
 
