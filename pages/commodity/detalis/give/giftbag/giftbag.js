98abfcf97c2750992c4928894b8eaa4488a142c3
ICEY-I-SEE <1141839113@qq.com>
2019-08-08 17:17:03 +0800

    转发

--- pages/commodity/detalis/give/giftbag/giftbag.js
+++ pages/commodity/detalis/give/giftbag/giftbag.js
@@ -126,9 +126,11 @@ Page({
     console.log('分享URL：',url)
     var nickname = app.globalData.userInfo.nickname;
     nickname = nickname == undefined ? '' : nickname;
-    wx.switchTab({
-      url: '/pages/index/index'
-    })
+    setTimeout(function(){
+      wx.switchTab({
+        url: '/pages/index/index'
+      })
+    },2000)
     return {
       title: nickname + '为你准备了一份惊喜,请火速查收!',
       imageUrl: 'https://giftbox.zhifengwangluo.com/image/back.png',
