0eec910799ef588378dfc48df02a66a1590f240b
ZZZhangYunfei <86975943@qq.com>
2019-08-14 19:13:06 +0800

    享物圈消息部分

--- pages/message/tupiandetail/tupiandetail.js
+++ pages/message/tupiandetail/tupiandetail.js
@@ -1,6 +1,7 @@
-
+var WxParse = require('../../../wxParse/wxParse.js');
 var api = require('../../../utils/api')
 var app = getApp();
+
 Page({
 
   /**
@@ -26,6 +27,7 @@ Page({
        console.log(res.data.data)
         that.setData({priture: res.data.data.cover})
         that.setData({content:res.data.data})
+        WxParse.wxParse('content', 'html', res.data.data.content, that, 5)
       }
     })
   },
@@ -46,14 +48,14 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+    this.setData({ autoplay: true })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+    this.setData({autoplay:false})
   },
 
   /**
