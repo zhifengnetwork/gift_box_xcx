46d39b690e587aa362d40c3e3e5c653072433cf4
ZZZhangYunfei <86975943@qq.com>
2019-08-19 16:19:30 +0800

    商品详情图片可以放大

--- pages/commodity/detalis/detalis.js
+++ pages/commodity/detalis/detalis.js
@@ -124,6 +124,18 @@ Page({
     })
 
   },
+  // 轮播图图片放大
+  //图片点击事件
+  imgYu: function(event) {
+    var src = event.currentTarget.dataset.src;//获取data-src
+    var imgList = event.currentTarget.dataset.list;//获取data-list
+    //图片预览
+    wx.previewImage({
+        current: src, // 当前显示图片的http链接
+        urls: imgList // 需要预览的图片http链接列表
+    })
+    
+  },
 
   /**赠送他人 */
   give_goods: function (e) {
