5113268dca8b020a538762f742c37e26d42c70dd
fuzhijingcom <yudwcom@163.com>
2019-09-17 12:47:09 +0800

    个人中心

--- pages/my/my.js
+++ pages/my/my.js
@@ -171,4 +171,35 @@ Page({
       url: 'after/after',
     })
   },
+
+  bindGetUserInfo:function(){
+    var that = this
+    // 必须是在用户已经授权的情况下调用
+    wx.getUserInfo({
+      success: function (res) {
+       
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        var province = userInfo.province
+        var city = userInfo.city
+        var country = userInfo.country
+
+        api.postJSON('api/login/update_userinfo', {
+          token: app.globalData.token,
+          nickname: nickName,
+          avatar: avatarUrl,
+          gender: gender,
+          province: province,
+          city: city
+
+        }, function (res) {
+          
+          app.globalData.userInfo = res.data.data;
+          that.onShow();
+        })
+      }
+    })
+  }
 })
